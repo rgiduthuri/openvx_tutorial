d2ab6bf10688cb5df4b1b1f3884963cb08496f36
Radha Giduthuri <rgiduthuri@gmail.com>
2016-03-13 15:47:31 -0700

    downloads mjpeg video sequence to workaround issues with OS X

--- scripts/download-videos.sh
+++ scripts/download-videos.sh
@@ -2,7 +2,8 @@
 # tutorial_videos folder to be under the openvx_tutorial folder to be able
 # pick the default video sequence when launching from Qt Creator
 #
-\rm -rf tutorial_videos.tgz tutorial_videos
-wget http://ewh.ieee.org/r6/scv/sps/openvx-material/tutorial_videos.tgz
-tar xvfz tutorial_videos.tgz
-\rm -rf tutorial_videos.tgz
+\rm -rf tutorial_videos
+mkdir tutorial_videos
+cd tutorial_videos
+wget http://ewh.ieee.org/r6/scv/sps/openvx-material/PETS09-S1-L1-View001.avi
+cd ..
